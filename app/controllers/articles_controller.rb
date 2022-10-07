9f15a39cf894a61f9743b3e7a7cc48cb32a038aa
Luca Tiengo <l.tiengo@greemm.com>
2022-10-07 17:18:40 +0200

    Alpha-blog completato

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
-
+  
+  before_action:set_article, only: [:show,:edit,:update,:destroy]
+  
   def show
-    #byebug
-    @article =  Article.find(params[:id])
+  
   end
 
   def index
@@ -13,8 +14,13 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
    
+  def edit
+
+  end
+
+  
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description, :body, :subtitle))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Articolo salvato correttamente"
       redirect_to article_path(@article) #pesca in automatico l'id dell'articolo appena creato e esegue la route show 
@@ -23,6 +29,32 @@ class ArticlesController < ApplicationController
     end 
   end
 
+  def update
+
+    if @article.update(article_params)
+      flash[:notice] = "Articolo aggiornato correttamente"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+
+    @article.destroy
+    redirect_to articles_path 
+  end
 
 
+  private 
+  
+  def set_article
+    @article =  Article.find(params[:id])
+  end
+
+
+  def article_params
+    params.require(:article).permit(:title, :description, :body, :subtitle)
+  end
 end
