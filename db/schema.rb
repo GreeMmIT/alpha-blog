3266d485588aa913aa79a17ea76804abf9284638
Luca Tiengo <l.tiengo@greemm.com>
2022-10-13 14:08:15 +0200

    Add users tabele and column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_133927) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_120024) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_133927) do
     t.datetime "updated_at", null: false
     t.string "body"
     t.string "subtitle"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "email"
   end
 
 end
