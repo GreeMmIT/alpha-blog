db230872251c866eff92b0d3913615fb90da85a4
Luca Tiengo <l.tiengo@greemm.com>
2022-10-17 10:37:04 +0200

    Pagination list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show 
     @user = User.find(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -13,7 +13,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
- 
+  def index 
+    @users =  User.paginate(page: params[:page], per_page: 5)
+  end
 
   def create
     @user = User.new (user_params)
@@ -30,7 +32,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Account aggiornato correttamente!"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
