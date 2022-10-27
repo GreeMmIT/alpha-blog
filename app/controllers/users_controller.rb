b5b7b32ee5763493eb6ee4589493ac3e242e3349
Luca Tiengo <l.tiengo@greemm.com>
2022-10-27 10:45:02 +0200

    user restrict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 before_action :set_user, only: [:show, :edit,:update]
-
-
+before_action :require_user, only: [:edit, :update]
+before_action :require_same_user , only: [:edit,:update]
   def show 
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
@@ -47,6 +47,13 @@ private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert]="Deve essere lo stesso utente per modificare"
+      redirect_to @user
+    end
   end
 
 end
